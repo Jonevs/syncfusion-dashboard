917457da7e4575270528cbeb2245b873f8de6e1f
Jonevs <jonathan.sampurna@gmail.com>
2022-08-10 12:51:26 +0700

    add pages

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Page,
+  Search,
+  Selection,
+  Inject,
+  Edit,
+  Toolbar,
+  Sort,
+  Filter,
+} from "@syncfusion/ej2-react-grids";
+import { customersData, customersGrid } from "../data/dummy";
+import { Header } from "../components";
 
 const Customers = () => {
-  return <div>Customers</div>;
+  const toolbarOptions = ["Delete", "Search"];
+  const editing = { allowDeleting: true, allowEditing: true };
+
+  return (
+    <div className="m-2 mt-24 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Customers" />
+      <GridComponent
+        dataSource={customersData}
+        allowPaging
+        allowSorting
+        toolbar={toolbarOptions}
+        editSettings={editing}
+        width="auto"
+      >
+        <ColumnsDirective>
+          {customersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject
+          services={[Page, Toolbar, Search, Selection, Edit, Sort, Filter]}
+        />
+      </GridComponent>
+    </div>
+  );
 };
 
 export default Customers;
--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -12,18 +12,45 @@ import {
   ExcelExport,
   Edit,
   Inject,
+  Toolbar
 } from "@syncfusion/ej2-react-grids";
 
-import { ordersData, contextMenuItems, ordersGrid } from "../data/dummy";
+import { ordersData, ordersGrid } from "../data/dummy";
 import { Header } from "../components";
 
 const Orders = () => {
   return (
     <div className="m-2 mt-24 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
       <Header category="Page" title="Orders" />
-      
+      <GridComponent
+        id="gridcomp"
+        dataSource={ordersData}
+        allowPaging
+        allowSorting
+        allowPdfExport
+        toolbar={["PdfExport"]}
+      >
+        <ColumnsDirective>
+          {ordersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject
+          services={[
+            Resize,
+            Sort,
+            ContextMenu,
+            Filter,
+            Page,
+            ExcelExport,
+            PdfExport,
+            Edit,
+            Toolbar
+          ]}
+        />
+      </GridComponent>
     </div>
-  )
+  );
 };
 
 export default Orders;
